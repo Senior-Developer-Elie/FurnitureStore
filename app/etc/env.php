fa3916113cb92f9db36a71bda67f4be4ddf62ef3
Senior-Developer-Elie <eliekassis.dev@gmail.com>
2021-07-15 12:41:17 +0900

    init master branch

---
+++ app/etc/env.php
@@ -0,0 +1,71 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_my'
+    ],
+    'crypt' => [
+        'key' => '78f52c2e0bca7d11045c666c0643b4dc'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'm2_quickstart',
+                'username' => 'root',
+                'password' => '',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '5d6_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '5d6_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'google_product' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        'local.furniturestore.com'
+    ],
+    'install' => [
+        'date' => 'Fri, 09 Jul 2021 19:28:31 +0000'
+    ]
+];
