f6148dc3efcd5c4c47589e4f1a3884c9064b7f01
Senior-Developer-Elie <eliekassis.dev@gmail.com>
2021-07-16 10:35:29 +0100

    permission change

--- app/etc/env.php
+++ app/etc/env.php
@@ -11,9 +11,9 @@ return [
         'connection' => [
             'default' => [
                 'host' => 'localhost',
-                'dbname' => 'm2_quickstart',
-                'username' => 'root',
-                'password' => '',
+                'dbname' => 'kempsey1_furniturestore',
+                'username' => 'kempsey1_user',
+                'password' => 'kempseypassword',
                 'active' => '1'
             ]
         ]
